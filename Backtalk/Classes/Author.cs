a44c1ee1f05fd050dac4eb9991be4eaf81e35098
Kaiser Sherman <ksherman@appliedinnovationinc.com>
2026-04-07 14:25:35 -0700

    Fix author list bug with changing keys causing bad UX and also causing the pfp to clear after a change to the author details. Added new Guid key to the Author.cs that doesn't change and is automatically set on the creation of a new Author instance.

--- Backtalk/Classes/Author.cs
+++ Backtalk/Classes/Author.cs
@@ -3,6 +3,7 @@
     public class Author
     {
         public int index { get; set; }
+        public Guid key { get; set; } = Guid.NewGuid();
         public string name { get; set; } = "";
         public string? pfp_b64 { get; set; }
         public string? role { get; set; }
