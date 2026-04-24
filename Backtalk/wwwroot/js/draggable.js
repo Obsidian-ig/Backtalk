b9b8dab1215ad4b4084810bd8fbf6957b83c908c
Kaiser Sherman <ksherman@appliedinnovationinc.com>
2026-04-24 16:30:53 -0700

    More work on project editor and import GSAP for dragging elements in the editor.

---
+++ Backtalk/wwwroot/js/draggable.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(Draggable, InertiaPlugin);
+});
+
+window.initDraggable = (element) => {
+    Draggable.create(element, {
+        type: "x,y",
+        edge
+    });
+}
