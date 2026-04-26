3dfc8821ad7068b8b60743f24485ce7eae1cdea7
Obsidian-ig <149707463+Obsidian-ig@users.noreply.github.com>
2026-04-26 01:08:44 -0700

    More work on Project Editor page. Made the tabs and stuff, also made the containers/explorers draggable. And some other stuff too.

--- Backtalk/wwwroot/js/draggable.js
+++ Backtalk/wwwroot/js/draggable.js
@@ -2,9 +2,24 @@
     gsap.registerPlugin(Draggable, InertiaPlugin);
 });
 
-window.initDraggable = (element) => {
-    Draggable.create(element, {
-        type: "x,y",
-        edge
-    });
-}
+window.gsapInterop = {
+    makeDraggable: function (selector) {
+        if (!window.gsap || !window.Draggable) {
+            console.error("GSAP or Draggable not loaded");
+            return;
+        }
+
+        gsap.registerPlugin(Draggable);
+
+        return Draggable.create(selector, {
+            type: "x,y",
+            edgeResistance: 0.65,
+            inertia: false,
+            cursor: "inherit",
+            dragClickables: false,
+            onDrag: function () {
+                console.log("onDrag!");
+            }
+        });
+    }
+};
