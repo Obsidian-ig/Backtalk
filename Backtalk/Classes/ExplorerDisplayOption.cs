1caa426daaf18833d7809d2629f343c942cbe1b1
Kaiser Sherman <ksherman@appliedinnovationinc.com>
2026-05-04 16:30:02 -0700

    Create a bunch of sorting functions for the endpoints. Also create a displayOption enum for the folder or endpoints to sort them by certain types.

---
+++ Backtalk/Classes/ExplorerDisplayOption.cs
@@ -0,0 +1,15 @@
+﻿namespace Backtalk.Classes
+{
+    public enum ExplorerDisplayOption
+    {
+        HIDDEN,
+        SORT_BY_NAME,
+        SORT_BY_NAME_REVERSED,
+        SORT_BY_TYPE,
+        SORT_BY_TYPE_REVERSED,
+        SORT_BY_CREATION_DATE,
+        SORT_BY_CREATION_DATE_REVERSED,
+        SORT_BY_LAST_MODIFIED,
+        SORT_BY_LAST_MODIFIED_REVERSED
+    }
+}
