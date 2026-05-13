ea1a622b110d7989e1376e365dd1c9874e824206
Kaiser Sherman <ksherman@appliedinnovationinc.com>
2026-05-13 16:29:39 -0700

    Start working on the folder items context menu in the endpoints explorer. Get the design almost completely finished. Just need to make it actually functional now.

--- Backtalk/Classes/ProjectData/ProjectFolder.cs
+++ Backtalk/Classes/ProjectData/ProjectFolder.cs
@@ -6,5 +6,6 @@
         public List<string> endpoints { get; set; } = new(); //list of all the included endpoints guids
         public DateTime created_at_utc { get; set; } = DateTime.UtcNow;
         public DateTime last_updated_utc { get; set; } = DateTime.UtcNow;
+        public ExplorerDisplayOption? endpoints_display_option { get; set; } = null;
     }
 }
