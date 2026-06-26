a9ff2507c8059f0636a8106005a67bfcfe1bae49
Kaiser Sherman <ksherman@appliedinnovationinc.com>
2026-06-26 15:10:26 -0700

    Refactor event handling and improve component cleanup
    Refactored JavaScript event listener management by introducing `ClickLibrary` for dynamic handling of `DotNetObjectReference` instances, reducing redundancy and preventing memory leaks. Updated .NET components to align with the new API, including renaming `objRef` to `_objRef` and adding `DisposeAsync` methods for proper cleanup.
    
    Improved percentage calculations in `ProjectItem.razor` with accurate rounding and fixed UI issues like `z-index` conflicts. Enhanced context menu behavior and simplified menu logic in `Projects.razor` for better maintainability. Added JavaScript initialization in `MainLayout.razor` to ensure proper setup.
    
    Updated demo project data to reflect new logic and reintroduced `UnfocusNameInput` in `ExplorerFolderItem.razor` with updated implementation. Addressed binary file changes in `.futdcache.v2`.

--- Backtalk/wwwroot/js/events.js
+++ Backtalk/wwwroot/js/events.js
@@ -1,5 +1,27 @@
-﻿window.setupClickEventListener = (dotNetHelper, methodName) => {
-    window.addEventListener('click', (event) => {
-        dotNetHelper.invokeMethodAsync(methodName);
-    });
-};
+﻿let dotNetObjectReferences = [];
+
+window.ClickLibrary = {
+    init: function (methodName, dotNetRef) {
+        window.addEventListener('click', this.onClick);
+    },
+    apply: function (methodName, dotNetRef) {
+        let exists = dotNetObjectReferences.some(item => item.ref === dotNetRef && item.method === methodName);
+        if (!exists) {
+            dotNetObjectReferences.push({ ref: dotNetRef, method: methodName });
+        }
+    },
+    onClick: function () {
+        for (const dotNetAndMethod of dotNetObjectReferences) {
+            try {
+                if (dotNetAndMethod.ref) {
+                    dotNetAndMethod.ref.invokeMethodAsync(dotNetAndMethod.method);
+                }
+            } catch (error) {
+                console.log("Skipped a bad dotNetRef.");
+            }
+        }
+    },
+    cleanup: function (methodName, dotNetRef) {
+        dotNetObjectReferences = dotNetObjectReferences.filter(item => !(item.ref === dotNetRef && item.method === methodName));
+    }
+}
